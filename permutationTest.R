c17d34ee4501a3a876490cad8c359ebd36666e8d
matasV99 <70501272+matasV99@users.noreply.github.com>
2024-03-16 18:20:02 +0800

    Update permutationTest.R
    Updated Eugene's code to include a timer and also iterate over all columns of NMM

--- permutationTest.R
+++ permutationTest.R
@@ -1,7 +1,7 @@
 permutationTest <- function(
     
   ## INPUT
-  NNMatrixVector,
+  NNM,
   cell_labels,
   
   alternative = 'greater',
@@ -19,46 +19,56 @@ permutationTest <- function(
     break
   }
   
-  obs <- by(NNMatrixVector, cell_labels, sum)
-  obs <- setNames(as.numeric(obs), names(obs))
-  
-  set.seed(seed)
-  nulldist <- list()
-  for(px in 1:nPerms){
-    if(verbose){
-      if(px==1 | px %% 100 == 0){
-        cat(paste0(px, ' of ', nPerms, '...'))
+  start_time <- Sys.time()
+  l_o <- list()
+  for (c in unique(cell_labels)){
+    NNMatrixVector <- NNM[,c]
+    obs <- by(NNMatrixVector, cell_labels, sum)
+    obs <- setNames(as.numeric(obs), names(obs))
+    
+    set.seed(seed)
+    nulldist <- list()
+    for(px in 1:nPerms){
+      if(verbose){
+        if(px==1 | px %% 100 == 0){
+          cat(paste0(px, ' of ', nPerms, '...'))
+        }
       }
+      null_labs <- sample(cell_labels, size = length(cell_labels), replace = F)
+      expx <- by(NNMatrixVector, null_labs, sum)
+      expx <- setNames(as.numeric(expx), names(expx))
+      nulldist[[px]] <- expx
     }
-    null_labs <- sample(cell_labels, size = length(cell_labels), replace = F)
-    expx <- by(NNMatrixVector, null_labs, sum)
-    expx <- setNames(as.numeric(expx), names(expx))
-    nulldist[[px]] <- expx
+    nulldist <- do.call(cbind, nulldist)
+    mean_exp <- apply(nulldist, 1, mean)
+    sd_exp <- apply(nulldist, 1, sd)
+    
+    pvals <- sapply(1:nrow(nulldist), function(j){
+      pval = 1 - sum( obs[j] < nulldist[j,]) / nPerms
+      if(alternative == 'greater'){
+        pval = 1 - pval
+      }
+      if(alternative == 'two-tailed'){
+        pval = pmin(1 - pval, pval)
+        pval = pval * 2
+      }
+      return(pval)
+    })
+    
+    OUT <-  data.frame(
+      'OBS' = obs,
+      'OBS PERCENT' = round(100 * obs / sum(obs), digits=2), 
+      'MEAN EXP' = mean_exp,
+      'SD EXP' = sd_exp,
+      'Z' = (obs - mean_exp)/sd_exp,
+      'PVAL' = pvals,
+      'SIG' = c('***', '**', '*', '.', '')[as.integer(cut(pvals, c(1, 0.1, 0.05, 0.01, 0.001, -1)))]
+    )
+    l_o[[c]] <- OUT
   }
-  nulldist <- do.call(cbind, nulldist)
-  mean_exp <- apply(nulldist, 1, mean)
-  sd_exp <- apply(nulldist, 1, sd)
-  
-  pvals <- sapply(1:nrow(nulldist), function(j){
-    pval = 1 - sum( obs[j] < nulldist[j,]) / nPerms
-    if(alternative == 'greater'){
-      pval = 1 - pval
-    }
-    if(alternative == 'two-tailed'){
-      pval = pmin(1 - pval, pval)
-      pval = pval * 2
-    }
-    return(pval)
-  })
-  
-  OUT <-  data.frame(
-    'OBS' = obs,
-    'OBS PERCENT' = round(100 * obs / sum(obs), digits=2), 
-    'MEAN EXP' = mean_exp,
-    'SD EXP' = sd_exp,
-    'Z' = (obs - mean_exp)/sd_exp,
-    'PVAL' = pvals,
-    'SIG' = c('***', '**', '*', '.', '')[as.integer(cut(pvals, c(1, 0.1, 0.05, 0.01, 0.001, -1)))]
-  )  
-  return(OUT)
+  combined_df <- do.call("rbind", l_o)
+  end_time <- Sys.time()
+  time_elapsed <- end_time - start_time
+  print(paste("Total time elapsed:", time_elapsed))
+  return(combined_df)
 }
